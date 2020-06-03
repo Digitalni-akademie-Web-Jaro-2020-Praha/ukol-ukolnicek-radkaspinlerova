a63766e4ea10c69dbe2e1033d8c66c2d9b85c715
RadkaSpin <radka.spinlerova@gmail.com>
2020-06-03 14:26:13 +0200

    vypracovany ukol

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 'use strict';
 
 console.log('funguju!');
+
+const tasks = ['Vyprat ponožky', 'Naučit se funkce vyššího řádu', 'Nakoupit na víkend', 'Udělat konečně úkol do JS', 'Vzít trio na výlet'];
+
+const updateTasks = () => {
+  const tasksElm = document.querySelector('.todo__tasks');
+  tasksElm.innerHTML = '';
+  for (let i = 0; i < tasks.length; i++) {
+    tasksElm.innerHTML += `<div class="task">${tasks[i]}</div>`;
+  }
+}
+updateTasks()
+
+const addNewTask = () => {
+  const input = document.querySelector('#new-task').value;
+  if (input === '') {
+    alert("Zapiš úkol do prázdného pole a kliknutím na tlačítko ho přidej na seznam.");
+  } else {
+    updateTasks(tasks.push(input))
+  }
+}
+
+const btnAdd = document.querySelector('.btn-add');
+btnAdd.addEventListener('click', addNewTask);
